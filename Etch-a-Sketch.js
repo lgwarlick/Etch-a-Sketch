15f94b18b51838263b4532037f9cf394760b7eb3
Logan <lgwarlick@gmail.com>
2020-02-19 11:15:45 -0600

    grid generated based off user input, start/reset buttons functional

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,19 +1,12 @@
-let gridWidth = 16;
-let gridHeight = 16;
-
+let gridWidth;
+let gridHeight;
 
 const gridContainer = document.querySelector('#gridContainer');
 
+let allCells = document.getElementsByClassName('cell');
 
 
-
-
-
-
-
-function generateGrid(gridWidth, gridHeight){ 
-    
-    const gridContainer = document.querySelector('#gridContainer');
+function generateGrid(gridWidth, gridHeight) { 
     
     for(let yValue = 0; yValue < gridHeight; yValue++) {
         
@@ -24,6 +17,9 @@ function generateGrid(gridWidth, gridHeight){
             
             let cell = document.createElement('div');
             cell.classList.add('cell');
+            
+            addListeners(cell);
+            
             row.appendChild(cell);
         }
         gridContainer.appendChild(row);
@@ -31,5 +27,50 @@ function generateGrid(gridWidth, gridHeight){
 
 }
 
-generateGrid(gridWidth, gridHeight);
+function addListeners(cell) {
+    cell.addEventListener('mouseenter', (e) => {
+        e.target.style.backgroundColor = 'black';
+    });
 
+    cell.addEventListener('mouseleave', (e) => {
+        e.target.style.backgroundColor = 'grey';
+    });
+    
+}
+
+
+const resetButton = document.querySelector('#resetButton');
+
+resetButton.addEventListener('click', () => {
+    
+    while(allCells.length == 0) {
+        return;
+    }
+    
+    for(let i = 0; i < allCells.length; i++) {
+        allCells[i].style.backgroundColor = "aquamarine";
+    }
+    
+});
+
+
+const startButton = document.querySelector('#startButton');
+
+startButton.addEventListener('click', () => {
+        
+        if(allCells.length !== 0) {
+            let child = gridContainer.lastElementChild;
+            while(child) {
+                gridContainer.removeChild(child);
+                child = gridContainer.lastElementChild;
+            }
+        }      
+    
+        gridWidth = prompt("Grid width?");
+        gridHeight = prompt("Grid height?");
+        generateGrid(gridHeight, gridWidth);     
+});
+
+/*
+
+*/
